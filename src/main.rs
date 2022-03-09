c3d4a1e6446f1c9650cc062d0fde7e03f43244c9
Stephen Merity <smerity@smerity.com>
2022-03-08 16:57:18 -0800

    Adding sitemap support, making whitespace more flexible, and cleaner end of file (EOF) handling for the parser

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,8 @@ use nom::sequence::preceded;
 use nom::{IResult};
 use nom::bytes::complete::{take_while, tag_no_case, tag};
 use nom::character::complete::{line_ending};
-use nom::combinator::opt;
-use nom::multi::many0;
+use nom::combinator::{opt, eof};
+use nom::multi::{many_till};
 
 struct UserAgent<'a> {
     pub agent: &'a [u8],
@@ -26,10 +26,15 @@ struct CrawlDelay {
     pub delay: Option<u32>,
 }
 
+struct Sitemap<'a> {
+    pub url: &'a [u8],
+}
+
 enum Line<'a> {
     UserAgent(UserAgent<'a>),
     Allow(Allow<'a>),
     Disallow(Disallow<'a>),
+    Sitemap(Sitemap<'a>),
     CrawlDelay(CrawlDelay),
     Raw(&'a [u8]),
 }
@@ -49,6 +54,9 @@ impl fmt::Debug for Line<'_> {
             Line::CrawlDelay(c) => f.debug_struct("CrawlDelay")
                 .field("delay", &c.delay)
                 .finish(),
+            Line::Sitemap(sm) => f.debug_struct("Sitemap")
+                .field("url", &sm.url.as_bstr())
+                .finish(),
             Line::Raw(r) => f.debug_struct("Raw")
                 .field("text", &r.as_bstr())
                 .finish(),
@@ -65,12 +73,7 @@ fn is_not_line_ending_or_comment(c: u8) -> bool {
 }
 
 fn line(input: &[u8]) -> IResult<&[u8], Line> {
-    //is_alphabetic)(input); //
     let (input, line) = take_while(is_not_line_ending)(input)?;
-    if line.is_empty() {
-        return Err(nom::Err::Error(nom::error::Error{ input, code: nom::error::ErrorKind::Eof }));
-    }
-    println!("line: {:?}", line.as_bstr());
     let (input, _) = opt(line_ending)(input)?;
     Ok((input, Line::Raw(line)))
 }
@@ -80,26 +83,32 @@ fn statement_builder<'a>(input: &'a [u8], target: &str) -> IResult<&'a [u8], &'a
     let (input, line) = take_while(is_not_line_ending_or_comment)(input)?;
     let (input, _) = opt(preceded(tag("#"), take_while(is_not_line_ending)))(input)?;
     let (input, _) = opt(line_ending)(input)?;
+    let line = line.trim();
     Ok((input, line))
 }
 
 fn user_agent(input: &[u8]) -> IResult<&[u8], Line> {
-    let (input, agent) = statement_builder(input, "user-agent: ")?;
+    let (input, agent) = statement_builder(input, "user-agent:")?;
     Ok((input, Line::UserAgent(UserAgent{ agent })))
 }
 
 fn allow(input: &[u8]) -> IResult<&[u8], Line> {
-    let (input, line) = statement_builder(input, "allow: ")?;
+    let (input, line) = statement_builder(input, "allow:")?;
     Ok((input, Line::Allow(Allow{ rule: line })))
 }
 
 fn disallow(input: &[u8]) -> IResult<&[u8], Line> {
-    let (input, line) = statement_builder(input, "disallow: ")?;
+    let (input, line) = statement_builder(input, "disallow:")?;
     Ok((input, Line::Disallow(Disallow{ rule: line })))
 }
 
+fn sitemap(input: &[u8]) -> IResult<&[u8], Line> {
+    let (input, url) = statement_builder(input, "sitemap:")?;
+    Ok((input, Line::Sitemap(Sitemap{ url })))
+}
+
 fn crawl_delay(input: &[u8]) -> IResult<&[u8], Line> {
-    let (input, time) = statement_builder(input, "crawl-delay: ")?;
+    let (input, time) = statement_builder(input, "crawl-delay:")?;
 
     let time= std::str::from_utf8(time).unwrap_or("1");
     let delay = match time.parse::<u32>() {
@@ -110,22 +119,26 @@ fn crawl_delay(input: &[u8]) -> IResult<&[u8], Line> {
 }
 
 fn robots_txt(input: &[u8]) -> IResult<&[u8], Vec<Line>> {
-    let (input, lines) = many0(alt((user_agent, allow, disallow, crawl_delay, line)))(input)?;
+    let (input, (lines, _)) = many_till(
+        alt((user_agent, allow, disallow, sitemap, crawl_delay, line)
+    ), eof)(input)?;
     Ok((input, lines))
 }
 
 fn main() {
     let txt = "Disallow: /path
 Allow: /path/exception
-Crawl-delay: 7";
+Crawl-delay: 60 # Very slow delay
 
-    println!("{}", txt);
-    println!();
+sitemap: https://example.com/sitemap.xml";
+
+    println!("{}\n", txt);
 
     let r = robots_txt(txt.as_bytes());
-    if let Ok((_, rlines)) = r {
+    if let Ok((_, rlines)) = &r {
         for (idx, line) in rlines.iter().enumerate() {
             println!("{}: {:?}", idx, line);
         }
     }
+    println!("\n{:?}", &r);
 }
